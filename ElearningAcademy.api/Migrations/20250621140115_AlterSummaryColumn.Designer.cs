ff6f29afb72c4401816c02067207a3d960ff9be8
aliMustafa52 <alimustafa6267@gmail.com>
2025-06-21 17:02:10 +0300

    ignore appsettings.json

--- ElearningAcademy.api/Migrations/20250621140115_AlterSummaryColumn.Designer.cs
+++ ElearningAcademy.api/Migrations/20250621140115_AlterSummaryColumn.Designer.cs
@@ -4,6 +4,7 @@ using ElearningAcademy.api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ElearningAcademy.api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250621140115_AlterSummaryColumn")]
+    partial class AlterSummaryColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ElearningAcademy.api/Migrations/20250621140115_AlterSummaryColumn.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElearningAcademy.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterSummaryColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Summary",
+                table: "Curriculums",
+                type: "nvarchar(2000)",
+                maxLength: 2000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Summary",
+                table: "Curriculums",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2000)",
+                oldMaxLength: 2000);
+        }
+    }
+}
